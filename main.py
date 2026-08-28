d1480b368f8bc397009af7fdd68466199b5216d3
pbword <prathameshpratham366@gmail.com>
2026-08-28 16:05:59 +0530

    Read endpoints with 404

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-from fastapi import FastAPI
+tasks = [
+    {"id": 1, "title": "Learn FastAPI", "done": False},
+    {"id": 2, "title": "Build CRUD API", "done": False},
+    {"id": 3, "title": "Deploy an AI project", "done": False},
+]
+
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI(
     title="Task API",
     description="A simple CRUD API built using FastAPI.",
     version="1.0"
 )
+
+@app.get("/tasks", summary="Get all tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/tasks/{id}", summary="Get a task by ID")
+def get_task(id: int):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {id} not found"
+    )
+
 
 
 @app.get("/", summary="Get API information")
